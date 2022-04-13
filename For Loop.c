67b04a8e3ed3733ac7ed0db0b496ffbbe06aa00e
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-04-13 12:29:38 +0530

    Create For Loop.c

---
+++ For Loop.c
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a,b;
+    cin>>a>>b;
+    
+      if(a <= 1 && a <= b)
+     printf("one\n");
+    if (a <= 2 && 2 <= b)
+     printf("two\n");
+    if (a <= 3 && 3 <= b)
+     printf("three\n");
+    if (a <= 4 && 4 <= b)
+     printf("four\n");
+    if (a <= 5 && 5 <= b)
+     printf("five\n");
+    if (a <= 6 && 6 <= b)
+     printf("six\n");
+    if (a <= 7 && 7 <= b)
+     printf("seven\n");
+    if (a <= 8 && 8 <= b)
+     printf("eight\n");
+    if(a <= 9 && 9 <= b)
+     printf("nine\n");
+    
+     if(b>=10)
+     {
+     for(int c = 10; c<= b; c++)
+     {
+     if(c%2 == 0)
+     printf("even\n");
+     else
+     printf("odd\n");
+     }
+     }
+     
+    return 0;
+}
