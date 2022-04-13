d4bdb180bc27b6a5d2b17ec000bf81e1852d27d9
divyas19 <73398403+divyas19@users.noreply.github.com>
2022-04-13 12:24:32 +0530

    Create Basic Data Types.cpp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    //cout<<a<<"\n"<<b<<"\n"<<c<<"\n"<<d<<"\n"<<e;
+    printf("%d\n",a);
+    printf("%ld\n",b);
+    printf("%c\n",c);
+    printf("%f\n",d);
+    printf("%lf\n",e);
+    return 0;
+}
